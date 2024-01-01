5a7a120b6e376addb875aa70679031a4affbe21e
Marismegai <154666609+Marismegai@users.noreply.github.com>
2024-01-01 23:55:36 +0700

    Update Fifao_Hub_v2_update.lua

--- Fifao_Hub_v2_update.lua
+++ Fifao_Hub_v2_update.lua
@@ -10,7 +10,7 @@ local Window = Fluent:CreateWindow({
     TabWidth = 160,
     Size = UDim2.fromOffset(530, 350),
     Acrylic = true, -- The blur may be detectable, setting this to false disables blur entirely
-    Theme = "Darker",
+    Theme = "Amethyst",
     MinimizeKey = Enum.KeyCode.End -- Used when theres no MinimizeKeybind
 })
 local Tabs = {
